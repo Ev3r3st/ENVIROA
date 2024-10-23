148e36f096a5cdb4a772f9c0d9af0195058785ee
Ev3r3st <matykrotil@gmail.com>
2024-10-23 10:31:40 +0200

    Refactor layout and home page components

---
+++ src/app/pages/welcomepage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WelcomePage: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My PWA App</h1>
+      <p>This is the welcome page of your Progressive Web App.</p>
+    </div>
+  );
+};
+
+export default WelcomePage;
